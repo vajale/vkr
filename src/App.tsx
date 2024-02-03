9c3617c2f9610a35fabb4a6d6c717d81abce8f3f
Иванов Константин Юрьевич <kivanov@ibs.ru>
2024-02-03 18:37:05 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { DesktopRouter } from "./routes";
+
+const App = () => {
+  return (
+    <div className="app">
+      <DesktopRouter />
+    </div>
+  );
+};
+
+export default App;
